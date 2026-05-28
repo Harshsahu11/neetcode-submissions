cc6c01f77699eca3519e9c40c0bf015adcfa7a82
HARSH SAHU <127614860+Harshsahu11@users.noreply.github.com>
2026-05-28 10:30:54 +0530

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.java
@@ -0,0 +1,45 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        int n = intervals.size();
+
+        int[] start = new int[n];
+        int[] end = new int[n];
+
+        for(int i=0;i<n;i++){
+            start[i]=intervals.get(i).start;
+            end[i]=intervals.get(i).end;
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+
+        int left = 0;
+        int right = 0;
+
+        int rooms = 0;
+        int maxRooms = 0;
+
+        while(left<n){
+            if(start[left]<end[right]){
+                rooms++;
+                left++;
+            }
+            else{
+                rooms--;
+                right++;
+            }
+            maxRooms = Math.max(rooms,maxRooms);
+        }
+        return maxRooms;
+    }
+}
